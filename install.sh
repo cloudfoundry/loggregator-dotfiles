9c2533e30bf0a5c998d1103d19fabbce14c15657
Eno Compton 4 <eno4@ecom.com>
2016-11-05 13:21:52 -0600

    Add installation steps for vim

--- install.sh
+++ install.sh
@@ -6,6 +6,11 @@ function link {
     ln -s $PWD/$1 $HOME/.$1
 }
 
+# pull in vundle (vim package manager)
+git submodule update --init
+# install vim plugins with vundle
+vim +PluginInstall +qall
+
 for dotfile in bashrc zshrc profile tmux.conf vimrc vim; do
     echo Linking $dotfile...
     link $dotfile
