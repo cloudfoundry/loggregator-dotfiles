0c1cb37b362edd5b3804446b0f0208ea8add0308
Eno Compton <eno4@ecom.com>
2016-11-07 19:32:44 -0700

    Improve install script

--- install.sh
+++ install.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
-set -e # exit on first failure
+all_dotfiles="bashrc zshrc profile tmux.conf vimrc vim aliases"
 
 function link {
-    ln -s $PWD/$1 $HOME/.$1
+    echo Attempting to link $1
+    ln -is $PWD/$1 $HOME/.$1
 }
 
-# pull in vundle (vim package manager)
-git submodule update --init
-# install vim plugins with vundle
-vim +PluginInstall +qall
+function print_error {
+    echo Failed to link $1
+}
 
-for dotfile in bashrc zshrc profile tmux.conf vimrc vim; do
-    echo Linking $dotfile...
-    link $dotfile
-done;
+function link_all_dotfiles {
+    for dotfile in $all_dotfiles; do
+        if [ -f $HOME/.$1 ];
+        then
+            # file exists
+            print_error $dotfile
+        else
+            # file does not exist
+            link $dotfile
+        fi
+    done
+}
 
-echo Done
+function initialize_vim_config {
+    # pull in vundle (vim package manager)
+    git submodule update --init
+    # install vim plugins with vundle
+    vim +PluginInstall +qall
+}
+
+initialize_vim_config
+link_all_dotfiles
