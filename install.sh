dfb6acc4ebb18bff69321732accc0e71dbc11289
Warren Fernandes <wfernandes@pivotal.io>
2016-12-02 09:30:49 -0700

    Add git-authors to dotfiles
    Signed-off-by: Eno Compton <ecompton@pivotal.io>

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-all_dotfiles="bashrc bash_darwin bash_profile zshrc common_profile tmux.conf vimrc vim aliases"
+all_dotfiles="bashrc bash_darwin bash_profile zshrc common_profile tmux.conf vimrc vim aliases git-authors"
 
 function link {
     echo Attempting to link $1
