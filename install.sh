d0e100ecd604a68973fdb31e4ef9d570ba27ad01
Brady Love <blove@pivotal.io>
2017-03-16 16:06:31 -0600

    Add alacritty config
    Signed-off-by: Eno Compton <ecompton@pivotal.io>

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-all_dotfiles="bashrc bash_darwin bash_profile common_profile tmux.conf vimrc vim aliases git-authors gitconfig Xresources"
+all_dotfiles="bashrc bash_darwin bash_profile common_profile tmux.conf vimrc vim aliases git-authors gitconfig Xresources alacritty.yml"
 
 function link {
     echo Attempting to link $1
