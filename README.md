8b7cbb86efccf55710cad6904f5ea0ddb9f034a7
Brady Love <blove@pivotal.io>
2018-02-12 12:59:09 -0700

    Add xubuntu-setup script
    Signed-off-by: Johanna Smith <josmith@pivotal.io>

--- README.md
+++ README.md
@@ -1,31 +1,15 @@
 # loggregator-dotfiles
 
-Shared dotfiles for the cloudfoundry-loggregator team
+## Install Loggregator Dotfiles
 
-We use an Ubuntu base with xfce4 on top.
-`sudo apt-get install xfce4-desktop xfce4-default-settings`
-
-Grab git to install the next few things from src.
-`sudo apt-get install git`
-
-Move on to install [alacritty](https://github.com/jwilm/alacritty#installation),
-vim: `sudo apt-get install vim`,
-ruby `sudo apt-get install ruby`,
-and [chruby](https://github.com/postmodern/chruby).
-
-Tmux dependencies:
 ```
-$ sudo apt install -y automake
-$ sudo apt install -y build-essential
-$ sudo apt install -y pkg-config
-$ sudo apt install -y libncurses5-dev
-$ sudo apt install -y libevent-dev
+./install.sh
 ```
 
-Now you should be all set to follow the rest of the [tmux](https://github.com/tmux/tmux) README to 
-```
-$ ./configure && make
-$ sudo make install
-```
+## Xubuntu Setup Script
 
-Finally, cd into `~/workspace/loggregator-dotfiles` and `./install.sh`.
+This will also install the Loggregator dotfiles.
+
+```
+./xubuntu-setup.sh
+```
---
+++ xubuntu-setup.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+set -ex
+
+sudo apt update
+sudo apt install -y automake build-essential pkg-config libncurses5-dev \
+  libevent-dev vim git ruby cmake libfreetype6-dev libfontconfig1-dev xclip curl \
+  chromium-browser python python-gtk2 python-xlib python-dbus python-wnck python-setuptools \
+  htop python-pip openssh-server
+
+curl https://sh.rustup.rs -sSf | sh
+source $HOME/.cargo/env
+
+mkdir ~/workspace
+
+pushd workspace
+  # Install alacritty
+  git clone https://github.com/jwilm/alacritty.git
+  pushd alacritty
+    cargo build --release
+    sudo cp target/release/alacritty /usr/local/bin
+    cp Alacritty.desktop ~/.local/share/applications
+  popd
+  rm -rf alacritty
+
+  # Install tmux
+  wget https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz
+  tar xzvf tmux-2.6.tar.gz
+  pushd tmux-2.6
+    ./configure
+    make
+    sudo make install
+  popd
+  rm tmux-2.6.tar.gz
+  rm -rf tmux-2.6
+
+  # Install chruby
+  wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
+  tar -xzvf chruby-0.3.9.tar.gz
+  pushd chruby-0.3.9/
+    sudo make install
+  popd
+  rm chruby-0.3.9.tar.gz
+  rm -rf chruby-0.3.9
+
+  wget -O ruby-install-0.6.1.tar.gz https://github.com/postmodern/ruby-install/archive/v0.6.1.tar.gz
+  tar -xzvf ruby-install-0.6.1.tar.gz
+  pushd ruby-install-0.6.1/
+    sudo make install
+  popd
+  rm ruby-install-0.6.1.tar.gz
+  rm -rf ruby-install-0.6.1
+
+  # Install Go
+  wget https://dl.google.com/go/go1.9.4.linux-amd64.tar.gz
+  tar xzvf go1.9.4.linux-amd64.tar.gz
+  sudo mv go /usr/local/go
+  rm go1.9.4.linux-amd64.tar.gz
+
+  # Install dotfiles
+  git clone https://github.com/cloudfoundry/loggregator-dotfiles.git
+  pushd loggregator-dotfiles
+    ./install.sh
+  popd
+
+  # Install VeraCrypt
+  wget https://launchpad.net/veracrypt/trunk/1.21/+download/veracrypt-1.21-setup.tar.bz2
+  mkdir veracrypt-setup
+  tar xvfj veracrypt-1.21-setup.tar.bz2 -C veracrypt-setup/
+  pushd veracrypt-setup
+    ./veracrypt-1.21-setup-gui-x64
+  popd
+  rm veracrypt-1.21-setup.tar.bz2
+  rm -rf veracrypt-setup
+
+  # Install quicktile
+  sudo pip install https://github.com/ssokolow/quicktile/archive/master.zip
+
+  # Install git-duet
+  mkdir git-duet
+  pushd git-duet
+    wget https://github.com/git-duet/git-duet/releases/download/0.5.2/linux_amd64.tar.gz
+    tar xzvf linux_amd64.tar.gz
+    cp git-* ~/bin/.
+  popd
+popd
